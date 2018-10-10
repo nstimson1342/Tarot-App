db71b5450e0fef07047be393d31c95b8a0d25a4a
William Hale <wmhale92@gmail.com>
2018-10-10 16:14:22 -0600

    fetched data and posted to front end

--- tarot-front-end/src/App.js
+++ tarot-front-end/src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      isLoaded: false,
-      card: {}
+  state = {
+    name: undefined,
+    desc: undefined
+  }
+
+  async componentDidMount() {
+    try {
+      const api_call = await fetch('http://localhost:5000/api/cards');
+      const data = await api_call.json()
+      console.log(data)
+      this.setState({
+        name: data[0].name,
+        desc: data[0].desc
+      })
+    } catch(err) {
+      console.log(err)
     }
   }
 
@@ -15,8 +26,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Its working! Its WORKING!!! this is tarot stuff</h1>
+        <p>{this.state.name}: {this.state.desc}</p>
       </div>
-
     );
   }
 }
