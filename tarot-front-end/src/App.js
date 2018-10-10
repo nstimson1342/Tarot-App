7c79dd6cd5fa5708fbc6f94ea8faa0f84748f91a
William Hale <wmhale92@gmail.com>
2018-10-10 15:00:29 -0600

    set up front end development environment

--- tarot-front-end/src/App.js
+++ tarot-front-end/src/App.js
@@ -10,35 +10,11 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    console.log("whatever")
-    fetch("/api/card/5bbbdf2b05896ab069e6b171")
-      .then(res => res.json())
-      .then(card => {
-        this.setState({card:card})
-          console.log(card)
-      })
 
-    //   .then(
-    //     (result) => {
-    //       console.log(result)
-    //       this.setState({
-    //         isLoaded: true,
-    //       });
-    //     },
-    //
-    //     (error) => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         error
-    //       });
-    //     }
-    //   )
-  }
   render() {
     return (
       <div className="App">
-        <img src={`https://gfx.tarot.com/images/site/decks/vampire/full_size/${this.state.card.value_int}.jpg`} alt="The Magician" />
+        <h1>Its working! Its WORKING!!! this is tarot stuff</h1>
       </div>
 
     );
